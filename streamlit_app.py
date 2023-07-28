7fb582dca601b2eeb12da1bfe2057b807f4d476a
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-07-28 21:51:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,12 @@ st.checkbox('Show probabilities')
 
 code = '''print('Hello world')'''
 st.code(code, language='python')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
