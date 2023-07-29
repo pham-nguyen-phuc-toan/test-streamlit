f2216b5c761ef5066ec5c884c0af3de818358122
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-07-29 21:19:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,66 @@
+# import streamlit as st
+# import tensorflow
+# import torch
+# import transformers
+# import openai
+
+# lang=st.selectbox("Select the Language of  the Solution:", ("Python", "C++", "Java"))
+# question=st.text_area("Input the Question Here")
+# button=st.button("Generate")
+
+# def response1(ques):
+#     openai.api_key=st.secrets["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"]
+    
+#     response = openai.Completion.create(
+#         model="code-cushman-001",
+#         prompt=f""""Give a {lang} solution for the Leetcode question 
+#                     Leetcode Question: {question}
+#                     {lang} Solution: """,
+#         temperature=0,
+#         max_tokens=1111,
+#         top_p=1,
+#         frequency_penalty=0,
+#         presence_penalty=0
+#         )
+#     print(response)
+#     return response.choices[0].text
+
+# if question and button:
+#     answer=response1(question)
+#     st.code(answer)
+
+# # from transformers import BertConfig, BertModel
+
+# # model = transformers.BertModel.from_pretrained("bert-base-uncased")
+# # st.write(model)
+
+# # pipe = transformers.pipeline("text-classification")
+
+# # st.write(pipe("This restaurant is awesome"))
+
+# st.title('Playground')
+
+# txt = st.text_area('Input prompt', '')
+
+# st.button('Submit')
+
+# temp = st.slider('Temperature', 0.00, 2.00, 1.00, 0.01, key=1)
+
+# top_p = st.slider('Top P', 0.00, 1.00, 1.00, 0.01, key=2)
+    
+# max_len = st.slider('Maximum length', 1, 4000, 1, 1, key=3)
+
+# st.checkbox('Show probabilities')
+
+# code = '''print('Hello world')'''
+# st.code(code, language='python')
+
 import streamlit as st
-import tensorflow
-import torch
-import transformers
-import openai
 
-lang=st.selectbox("Select the Language of  the Solution:", ("Python", "C++", "Java"))
-question=st.text_area("Input the Question Here")
-button=st.button("Generate")
+st.title('This is a next level SEO app')
 
-def response1(ques):
-    openai.api_key=st.secrets["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"]
-    
-    response = openai.Completion.create(
-        model="code-cushman-001",
-        prompt=f""""Give a {lang} solution for the Leetcode question 
-                    Leetcode Question: {question}
-                    {lang} Solution: """,
-        temperature=0,
-        max_tokens=1111,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-        )
-    print(response)
-    return response.choices[0].text
+st.write('Make your idea real')
 
-if question and button:
-    answer=response1(question)
-    st.code(answer)
+st.write('What is your seed keyword?')
 
-# from transformers import BertConfig, BertModel
-
-# model = transformers.BertModel.from_pretrained("bert-base-uncased")
-# st.write(model)
-
-# pipe = transformers.pipeline("text-classification")
-
-# st.write(pipe("This restaurant is awesome"))
-
-st.title('Playground')
-
-txt = st.text_area('Input prompt', '')
-
-st.button('Submit')
-
-temp = st.slider('Temperature', 0.00, 2.00, 1.00, 0.01, key=1)
-
-top_p = st.slider('Top P', 0.00, 1.00, 1.00, 0.01, key=2)
-    
-max_len = st.slider('Maximum length', 1, 4000, 1, 1, key=3)
-
-st.checkbox('Show probabilities')
-
-code = '''print('Hello world')'''
-st.code(code, language='python')
+st.text_input()
