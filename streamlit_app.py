3d9829e80ecfdc1eaa257181ced1e559fabffd26
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-08-09 22:26:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,6 @@ if st.button('Submit'):
     response = requests.post("https://d1b0-116-102-221-147.ngrok-free.app/chatgpt", headers=headers, params=params)
     response = response.json()
     result = response['result']
-    
+
+    st.write('Generated code')
     st.code(result)
