6997704725fc4aad4cae665bff0ef8083f81559f
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-08-22 22:21:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,20 +9,20 @@ img = st.file_uploader("Upload a file", type=(["png"]))
 if img is not None:
     vgg16_model = models.vgg16(pretrained=True)
     
-    # # Apply transformations to the image
-    # transform = transforms.Compose([
-    #     transforms.Resize(256),
-    #     transforms.CenterCrop(224),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(
-    #         mean=[0.485, 0.456, 0.406],
-    #         std=[0.229, 0.224, 0.225]
-    #     )
-    # ])
-    # img = transform(img)
+    # Apply transformations to the image
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+    img = transform(img)
     
-    # # Add a batch dimension to the image
-    # img = img.unsqueeze(0)
+    # Add a batch dimension to the image
+    img = img.unsqueeze(0)
     
     # Set the model to evaluation mode
     vgg16_model.eval()
